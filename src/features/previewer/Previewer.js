b2578ba28f3e2e574d8533077b320afc2d6fa7d3
Daniel Keicher <dnlkch86@gmail.com>
2022-11-11 17:48:25 +0100

    first commit, cleaned app files

--- src/features/previewer/Previewer.js
+++ src/features/previewer/Previewer.js
@@ -4,13 +4,11 @@ import {
   decrement,
   increment,
   incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from './previewerSlice';
+import styles from './Previewer.module.css';
 
-export function Counter() {
+export function Previewer() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
@@ -48,18 +46,6 @@ export function Counter() {
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
         </button>
       </div>
     </div>
